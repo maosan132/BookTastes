45c90393f6cde6c9e136da03ce7aff7cf18bb637
maosan132 <maosan132@gmail.com>
2020-10-15 17:03:55 -0500

    added linters under github/wfls folder

--- db/migrate/20201015200731_create_followings.rb
+++ db/migrate/20201015200731_create_followings.rb
@@ -2,7 +2,7 @@ class CreateFollowings < ActiveRecord::Migration[6.0]
   def change
     create_table :followings do |t|
       t.integer :follower_id
-      t.integer :followed_id
+      t.integer :followed_user_id
 
       t.timestamps
     end
